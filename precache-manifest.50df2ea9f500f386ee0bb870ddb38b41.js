3295ccff3e294cd96c83c0438609536ca56db835
Daniel Anadi <daniel@Daniels-MacBook-Pro-2.local>
2020-01-09 21:10:35 +0700

    Updates

---
+++ precache-manifest.50df2ea9f500f386ee0bb870ddb38b41.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a73e6ff0d56ef820bd1f9aedb4a2b1f6",
+    "url": "/webDesProject/index.html"
+  },
+  {
+    "revision": "299d75e45e09505bc21a",
+    "url": "/webDesProject/static/css/main.84757403.chunk.css"
+  },
+  {
+    "revision": "b9fde089fc1c52acf993",
+    "url": "/webDesProject/static/js/2.715fe25f.chunk.js"
+  },
+  {
+    "revision": "e928fe768baa9832b5bc57eae021f30c",
+    "url": "/webDesProject/static/js/2.715fe25f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "299d75e45e09505bc21a",
+    "url": "/webDesProject/static/js/main.947765c5.chunk.js"
+  },
+  {
+    "revision": "01274a0286332a01f1f8",
+    "url": "/webDesProject/static/js/runtime-main.aaa20a41.js"
+  },
+  {
+    "revision": "1311b681b0db742e1908938373c00401",
+    "url": "/webDesProject/static/media/MySLpromo.1311b681.mp4"
+  },
+  {
+    "revision": "e8342f0f40148008c4c42c32a46f4863",
+    "url": "/webDesProject/static/media/bg1.e8342f0f.png"
+  },
+  {
+    "revision": "1a260ced10c5564ab8d8ca1b2304831f",
+    "url": "/webDesProject/static/media/dp.1a260ced.jpg"
+  },
+  {
+    "revision": "475695db36e4289306917af446448ec5",
+    "url": "/webDesProject/static/media/logo.475695db.png"
+  },
+  {
+    "revision": "5e46323f147448ec091b6d012916fb38",
+    "url": "/webDesProject/static/media/nyeselkanloe.5e46323f.wav"
+  },
+  {
+    "revision": "13c6dbe44729de28c9520484515f7bda",
+    "url": "/webDesProject/static/media/profilebg.13c6dbe4.jpg"
+  },
+  {
+    "revision": "7641a8df6755b722929546e310958314",
+    "url": "/webDesProject/static/media/sally.7641a8df.png"
+  }
+]);
